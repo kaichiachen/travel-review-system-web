3079fd635fdcfd50953f7dceb0ea75eeac7a46b1
Andy <singbird81@gmail.com>
2017-12-10 13:29:04 +0800

    fix user bug

--- src/page/User.vue
+++ src/page/User.vue
@@ -1,51 +1,53 @@
 <template>
-<div class="userContainer">
-  <md-table v-once>
-    <md-table-header>
-        <md-table-row>
-            <md-table-head>姓名</md-table-head>
-            <md-table-head>账号</md-table-head>
-            <md-table-head>权限</md-table-head>
-            <md-table-head>删除</md-table-head>
-        </md-table-row>
-    </md-table-header>
+  <div class="userContainer">
+    <md-table>
+      <md-table-header>
+          <md-table-row>
+              <md-table-head>姓名</md-table-head>
+              <md-table-head>账号</md-table-head>
+              <md-table-head>权限</md-table-head>
+              <md-table-head>删除</md-table-head>
+          </md-table-row>
+      </md-table-header>
 
-    <md-table-body>
-        <md-table-row v-for="(row, rowIndex) in users" :key="rowIndex" :md-item="row">
-            <md-table-cell>{{ row.name }}</md-table-cell>
-            <md-table-cell>{{ row.username }}</md-table-cell>
-            <md-table-cell>
-              <md-select
-                placeholder="角色"
-                v-model="row.role"
-                @selected="changeRole(rowIndex)">
-                <md-option value="0">管理员</md-option>
-                <md-option value="1">终审者</md-option>
-                <md-option value="2">评论者</md-option>
-                <md-option value="3">一般使用者</md-option>
-              </md-select>
-            </md-table-cell>
-            <md-table-cell>
-              <md-button class="md-icon-button md-raised md-accent" @click="showDeleteDialog(row)">
-                X
-              </md-button>
-            </md-table-cell>
-        </md-table-row>
-    </md-table-body>
-  </md-table>
-  <md-dialog md-open-from="#custom" md-close-to="#custom" ref="dialog">
-    <md-dialog-title>确定要删除？</md-dialog-title>
-    <md-dialog-actions>
-      <md-button class="md-primary" @click="deleteUser()">是</md-button>
-      <md-button class="md-primary" @click="closeDeleteDialog()">否</md-button>
-    </md-dialog-actions>
-  </md-dialog>
-</div>
+      <md-table-body>
+          <md-table-row v-for="(row, rowIndex) in users" :key="rowIndex" :md-item="row">
+              <md-table-cell>{{ row.name }}</md-table-cell>
+              <md-table-cell>{{ row.username }}</md-table-cell>
+              <md-table-cell>
+                <md-select
+                  placeholder="角色"
+                  v-model="row.role"
+                  @closed="changeRole(rowIndex)">
+                  <md-option value="0">管理员</md-option>
+                  <md-option value="1">终审者</md-option>
+                  <md-option value="2">评论者</md-option>
+                  <md-option value="3">一般使用者</md-option>
+                </md-select>
+              </md-table-cell>
+              <md-table-cell>
+                <md-button class="md-icon-button md-raised md-accent" @click="showDeleteDialog(row)">
+                  X
+                </md-button>
+              </md-table-cell>
+          </md-table-row>
+      </md-table-body>
+    </md-table>
+    <md-dialog md-open-from="#custom" md-close-to="#custom" ref="dialog">
+      <md-dialog-title>确定要删除？</md-dialog-title>
+      <md-dialog-actions>
+        <md-button class="md-primary" @click="deleteUser()">是</md-button>
+        <md-button class="md-primary" @click="closeDeleteDialog()">否</md-button>
+      </md-dialog-actions>
+    </md-dialog>
+    <snackbar ref="snackbar"></snackbar>
+  </div>
 </template>
 
 <script>
 
-import { userListReq } from '@/service';
+import { userListReq, deleteUserReq } from '@/service';
+import snackbar from '@/components/SnackBar';
 
 export default {
   data: () => ({
@@ -56,6 +58,9 @@ export default {
   mounted() {
     this.initData();
   },
+  components: {
+    snackbar,
+  },
   methods: {
     initData() {
       userListReq().then((success) => {
@@ -64,14 +69,13 @@ export default {
           const res = success.User;
           for (let i = 0; i < res.length; i += 1) {
             this.users.push({
+              id: res[i].id,
               name: res[i].name,
               username: res[i].username,
               role: ''.concat(res[i].role),
             });
           }
         }
-        /* eslint no-console: ["error", { allow: ["debug"] }] */
-        console.debug(this.users.length);
       }, (error) => {
         /* eslint no-console: ["error", { allow: ["debug"] }] */
         console.debug(error);
@@ -79,19 +83,31 @@ export default {
     },
     showDeleteDialog(user) {
       this.nowuser = user;
-      /* eslint no-console: ["error", { allow: ["debug"] }] */
-      console.debug(user.role);
       this.$refs.dialog.open();
     },
     closeDeleteDialog() {
       this.$refs.dialog.close();
     },
     deleteUser() {
-      this.$refs.dialog.open();
+      deleteUserReq(this.nowuser.id).then((success) => {
+        if (success.message === 'success') {
+          this.$refs.snackbar.msg = '删除完毕';
+          this.$refs.snackbar.open();
+        } else {
+          this.$refs.snackbar.msg = '删除失败';
+          this.$refs.snackbar.open();
+        }
+        this.$refs.dialog.close();
+      }, (error) => {
+        /* eslint no-console: ["error", { allow: ["debug"] }] */
+        console.debug(error);
+      });
     },
     changeRole(index) {
-      /* eslint no-console: ["error", { allow: ["debug"] }] */
-      console.debug(index);
+      setTimeout(() => {
+        /* eslint no-console: ["error", { allow: ["debug"] }] */
+        console.debug(this.users[index].role);
+      }, 500);
     },
   },
 };
