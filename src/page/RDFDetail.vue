b25739bdf595ef495a9cdb2f376bfddc1d907da2
bugzyz <bugzyz@hotmail.com>
2017-12-28 10:51:50 +0800

    add the rdf-generator in page /rdf

---
+++ src/page/RDFDetail.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="reviewdetailcontainer">
+  <md-card md-with-hover>
+    <md-card-header>
+        <div class="time">生成时间：{{storetime}}</div>
+    </md-card-header>
+
+    <md-card-content>
+        {{ content}}
+    </md-card-content>
+  </md-card>  
+</div>
+</template>
+
+<script>
+
+
+export default {
+  data: () => ({
+    storetime: '',
+    content: '',
+  }),
+  components: {
+  },
+  methods: {
+  },
+};
+</script>
+
+<style>
+.mypasscard {
+  margin-top: 50px;
+}
+.myreviewcard {
+  margin-top: 50px;
+  margin-bottom: 50px;
+}
+</style>
